6cd28c53beb02ef7915928a817f24aff96b5b5e7
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-04-26 00:07:53 -0300

    Update conjvet.h

--- conjvet.h
+++ conjvet.h
@@ -1,4 +1,3 @@
-//                         !!!!!!!      IN CONSTRUCTION !!!!!!!
 #ifndef CONJVET_H
 #define CONJVET_H
 #include <string>
@@ -19,7 +18,9 @@ public:
     void intersec(conjvet x,conjvet y);
     void menas(conjvet x, conjvet y);
     int escalar(conjvet x, conjvet y);
-
+    bool subconj(conjvet x, conjvet y);
+    bool conjiden(conjvet x, conjvet y);
+    bool disjunt(conjvet x, conjvet y);
 
 };
 #endif
